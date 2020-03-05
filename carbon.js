f0dc8ba21e62f4645c8d7d52174bd834f2c4637c
zidniryi <zidnirydwan@gmail.com>
2020-03-05 09:12:58 +0700

    learn gesture tab

---
+++ carbon.js
@@ -0,0 +1,19 @@
+// @konsepkoding
+/**
+ * Cara Menemukan Sebuah Kata Pada Javascript.
+ * Dengan Mudah Dan Simple.
+ */
+const word = 'Jakarta Saat Ini Adalah Ibukota Dari Negara Indonesia'
+const findWord = word.includes('Jakarta')
+if (findWord) {
+    console.log('Sukses')
+} else {
+    console.log('Kata Tidak Ditemukan')
+}
+
+
+/**
+ * Cinta Pertama Itu Seperti Program Hello World
+ * Tidak Ada Bugs dan CLean Code
+ * Tapi Tidak Berguna Dan Biasanya Menyakitkan
+ */
